05e3e366e161550a7f9a7e3a15e10947e69679a3
ChrisKnowsDev <chrisharley81@gmail.com>
2024-06-24 10:23:13 -0400

    setup unit testing with jest

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
+import Link from 'next/link'
 import { Button } from '@/components/ui/button'
 
 export default function Home() {
   const currentYear = new Date().getFullYear()
   return (
-    <main className="p-8">
-      <h1 className="text-base font-thin tracking-wide md:text-2xl lg:text-4xl">
-        portfolio V2.0 by: chrisharley.io &copy; {currentYear}
-      </h1>
-      <Button>Fake Button</Button>
-    </main>
+    <div>
+      <h1>Home</h1>
+      <Button>
+        <Link href="/about">About</Link>
+      </Button>
+    </div>
   )
 }
